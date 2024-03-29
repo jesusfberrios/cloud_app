c358e5647080455b6ecc5da9a3e9381a0153570a
Jesus Berrios <81935591+jesusfberrios@users.noreply.github.com>
2024-03-29 00:20:55 -0400

    adjusting width and height of plot and figure

--- main/app.py
+++ main/app.py
@@ -64,7 +64,7 @@ def main():
     # Plot the data using Plotly
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=df['year'], y=df['population'], mode='lines+markers', name='Population'))
-    fig.update_layout(title='Population Trend', xaxis_title='Year', yaxis_title='Population',height=100)
+    fig.update_layout(title='Population Trend', xaxis_title='Year', yaxis_title='Population',height=300)
 
     # Specify x-axis tick values to show only integer years
     fig.update_xaxes(tickmode='array', tickvals=df['year'].unique(), tickformat='d')
@@ -78,7 +78,7 @@ def main():
 
     # Display image
     st.subheader("Province Map:")
-    st.image(BytesIO(image_data),width=300)
+    st.image(BytesIO(image_data),height=300)
 
     # # Display Plotly chart and image side by side
     # col1, col2 = st.columns([1, 1])  # Split the layout into two columns
